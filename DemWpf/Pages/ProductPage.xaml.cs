5adc64f1a73a6f7df7a1437f108b52e47cf3c3bd
221 <221@221-01>
2026-01-30 18:08:32 +0300

    +3

--- DemWpf/Pages/ProductPage.xaml.cs
+++ DemWpf/Pages/ProductPage.xaml.cs
@@ -22,6 +22,10 @@ namespace DemWpf.Pages
             
             LoadUser();
             LoadProducts();
+            LoadSuppliers();
+
+            SupplierFilterBox.SelectedIndex = 0;
+            SortBox.SelectedItem = 0;
         }
 
         private void LoadUser()
@@ -42,6 +46,58 @@ namespace DemWpf.Pages
                 .ToList();
 
             ProductsListView.ItemsSource = _products;
+        }
+
+        private void LoadSuppliers()
+        {
+            using var db = new Dem21Context();
+
+            SupplierFilterBox.Items.Clear();
+            SupplierFilterBox.Items.Add("Все поставщики");
+
+            var suppliers = db.Suppliers.Select(x => x.Supplier1);
+
+            foreach (var supplier in suppliers)
+            {
+                SupplierFilterBox.Items.Add(supplier);
+            }
+
+        }
+
+        private void FilterChanged(object sender, EventArgs e)
+        {
+            IEnumerable<Product> result = _products;
+
+            string search = SearchTextBox.Text;
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                result = result.Where(p =>
+                    p.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    p.Description.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    p.Manufacturer.Manufacturer1.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    p.Supplier.Supplier1.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    p.Category.Name.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    p.Article.Contains(search, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (SupplierFilterBox.SelectedIndex > 0)
+            {
+                string supplier = SupplierFilterBox.SelectedItem?.ToString();
+                result = result.Where(p => p.Supplier.Supplier1 == supplier);
+            }
+
+            switch (SortBox.SelectedIndex)
+            {
+                case 1:
+                    result = result.OrderBy(p => p.Amount); 
+                    break;
+                case 2:
+                    result = result.OrderByDescending(p => p.Amount);
+                    break;
+            }
+
+            ProductsListView.ItemsSource = result.ToList();
         }
     }
 }
